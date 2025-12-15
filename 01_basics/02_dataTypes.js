5f16794cdc8858413d7bda4d5dd748914be18656
AKIF ANSARI <81532066+dev-AAKIF@users.noreply.github.com>
2025-12-15 20:04:02 +0000

    02_dataTypes

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -1,29 +1,58 @@
+// "use strict"; // treat all JS code as newer version
+
+// // alert( 3 + 3) // we are using nodejs, not browser
+
+// console.log(3 
+//     +
+//      3) // code readability should be high
+
+// console.log("Akif")
+
+
+// let name = "akif"
+// let age = 18
+// let isLoggedIn = false
+// let state;
+
+// // number => 2 to power 53
+// // bigint
+// // string => ""
+// // boolean => true/false
+// // null => standalone value
+// // undefined => 
+// // symbol => unique
+
+
+// // object
+
+// console.log(typeof undefined); // undefined
+// console.log(typeof null); // object
+
 "use strict"; // treat all JS code as newer version
 
-// alert( 3 + 3) // we are using nodejs, not browser
+// alert(3 + 3); // we are using nodejs, not browser
 
-console.log(3 
-    +
-     3) // code readability should be high
+// console.log(3 
+//     + 
+//     3)  // code readability should be high
 
-console.log("Hitesh")
+// console.log("Akif")
 
-
-let name = "hitesh"
-let age = 18
-let isLoggedIn = false
-let state;
+let name = "Akif"
+let age = 21;
+let isLoggedIn = false;
 
 // number => 2 to power 53
 // bigint
-// string => ""
-// boolean => true/false
+// string = ""
+// boolean = true/false
 // null => standalone value
-// undefined => 
+// undefined
 // symbol => unique
+
 
 
 // object
 
-console.log(typeof undefined); // undefined
-console.log(typeof null); // object
+console.log(typeof undefined) //undefined
+console.log(typeof null) //object
