561b732c9ed1e55279caa565890cb0bfa69f6fd1
AKIF ANSARI <81532066+dev-AAKIF@users.noreply.github.com>
2025-12-15 17:52:23 +0000

    01_variables

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,22 +1,43 @@
-const accountId = 144553
-let accountEmail = "Akif@google.com"
-var accountPassword = "12345"
-accountCity = "Navi Mumbai"
+// const accountId = 144553
+// let accountEmail = "Akif@google.com"
+// var accountPassword = "12345"
+// accountCity = "Navi Mumbai"
+// let accountState;
+
+// // accountId = 2 // not allowed
+
+
+// accountEmail = "ak@ak.com"
+// accountPassword = "21212121"
+// accountCity = "Mumbai"
+
+// console.log(accountId);
+
+// /*
+// Prefer not to use var
+// because of issue in block scope and functional scope
+// */
+
+
+// console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+
+// */
+
+const accountId = 47;
+let accountEmail =  `dev.aakif@gmail.com`
+var accountPassword = `1234567890`
+accountCity = `Mumbai`
+
+// accountId = 20   // not allowed because we cannot redefine const
+accountEmail= "akif7868@gmail.com"
+accountPassword = '9999999999'
+accountCity = "Thane"
 let accountState;
-
-// accountId = 2 // not allowed
-
-
-accountEmail = "ak@ak.com"
-accountPassword = "21212121"
-accountCity = "Mumbai"
 
 console.log(accountId);
 
-/*
-Prefer not to use var
-because of issue in block scope and functional scope
+/* 
+Prefer not to use Var because of issue in Block Scope and Functional Scope
 */
 
-
-console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
