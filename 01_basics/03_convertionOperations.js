5a975ebe763593f15a173c4a74768bb47fb257f2
AKIF ANSARI <81532066+dev-AAKIF@users.noreply.github.com>
2025-12-16 20:05:02 +0000

    comparison

--- 01_basics/03_convertionOperations.js
+++ 01_basics/03_convertionOperations.js
@@ -44,14 +44,26 @@ let str1 = "Hello"
 let str2 = "Akif"
 
 let str3 = str1 + " " + str2
-console.log(str3)
+// console.log(str3)
 
-console.log("1" + 2)
-console.log(1 + "2");
-console.log("1" + 2 + 3);
-console.log(1 + 2 + "3");
+// console.log("1" + 2)
+// console.log(1 + "2");
+// console.log("1" + 2 + 3);
+// console.log(1 + 2 + "3");
 
+// console.log( 3 + 3 * 5 % 3)
+// console.log( (3 + 3) * 5 % 3)
 
+// console.log(+true)
+// console.log(+"")
 
+let num1, num2, num3
 
+num1 = num2 = num3 = 2 + 2
 
+let gameCounter = 105
+++gameCounter
+console.log(gameCounter);
+
+// link to study
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
