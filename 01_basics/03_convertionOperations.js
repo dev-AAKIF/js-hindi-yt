a72f0442d5e9425b6be1192bfb303192b9b900dd
AKIF ANSARI <81532066+dev-AAKIF@users.noreply.github.com>
2025-12-16 11:55:05 +0000

    conversion of data types

---
+++ 01_basics/03_convertionOperations.js
@@ -0,0 +1,29 @@
+let score = "Akif"
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number(score)
+
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => Nan
+// true => 1, false => 0
+
+let isLoggedIn = "sdfgh"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// 1 => True, 0 => False
+// "" => false
+// "akif" => true
+
+let someNumber= 47
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
