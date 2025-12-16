d7acac837fe8c13d859a63d2a77c46620c7de49a
AKIF ANSARI <81532066+dev-AAKIF@users.noreply.github.com>
2025-12-16 12:40:29 +0000

    datatypes pb

--- 01_basics/03_convertionOperations.js
+++ 01_basics/03_convertionOperations.js
@@ -24,6 +24,34 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 let someNumber= 47
 
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// ******************* Operations **************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "Hello"
+let str2 = "Akif"
+
+let str3 = str1 + " " + str2
+console.log(str3)
+
+console.log("1" + 2)
+console.log(1 + "2");
+console.log("1" + 2 + 3);
+console.log(1 + 2 + "3");
+
+
+
+
 
