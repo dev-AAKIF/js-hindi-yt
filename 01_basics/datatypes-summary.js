6e487cabcf12734fb058194ac838ba4b7ffa5641
AKIF ANSARI <81532066+dev-AAKIF@users.noreply.github.com>
2025-12-17 21:26:23 +0000

    updated

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -35,4 +35,36 @@ const myFunction = function() {
     console.log("Hello World");
 }
 
-console.log(typeof scoreValue);
+// console.log(typeof scoreValue);
+
+// https://262.ecma-international.org/5.1/#sec-11.4.3
+
+// +++++++++++++++++++++++++++++++++++++++++++=
+
+// Stack (Primitive), Heap (Non-Primitive)
+
+// Stack is used for Primitive Datatypes and Heap is used for Non-Primitive DataTypes
+
+// In Stack Memory, The Variable which we declare,we get a copy of that, and whatever changes we do to the updated variable, the changes are done to the updated memoried variable and not the original value
+
+// In Heap Memory, the Variable or object which we declare, we get the reference of the original value and whatever changes we do to the reference, the original value gets changed and it gets updated with that change
+
+let myYoutubeName = "akifansaridotcom"
+
+let updatedName = myYoutubeName
+updatedName = "chaiaurcode"
+
+// console.log(myYoutubeName);
+// console.log(updatedName);
+
+let userOne = {
+    email: "user@google.com",
+    upiId: "user@hdfcbank"
+}
+
+let userTwo = userOne
+
+userTwo.email = "akif@google.com"
+console.log(userOne.email);
+console.log(userTwo.email);
+
