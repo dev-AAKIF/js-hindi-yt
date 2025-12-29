d6d7592f62479e075dfb2a6b2d259cb42dcca084
AKIF ANSARI <81532066+dev-AAKIF@users.noreply.github.com>
2025-12-29 14:26:14 +0000

    arrow

--- 03_basics/03_arrows.js
+++ 03_basics/03_arrows.js
@@ -4,14 +4,79 @@ const user ={
 
     welcomeMessage: function() {
         console.log(`${this.username} , welcome to website`);
-        console.log(this);
+        // console.log(this);
         
         
     }
 }
 
-user.welcomeMessage()
-user.username = "Sam"
-user.welcomeMessage()
+// user.welcomeMessage()
+// user.username = "Sam"
+// user.welcomeMessage()
 
-console.log(this);
+// console.log(this);
+
+// function chai() {
+//     let username = "Akif"
+//     console.log(this.username);   
+// }
+
+// chai()
+// console.log(this);
+
+// +++++++++++ Arrow Function  +++++++
+
+{/*
+    instead of function chai() {
+        
+    }
+
+    or instead of 
+
+    const chai = function() {
+
+    }
+
+    ----------------- xxx ----------------
+
+    we declare arrow functions as below
+
+    () => {
+        
+    }
+
+    or
+
+    const chai = () => {
+        
+        }
+
+    */}
+
+const chai = () => {
+    let username = "Akboss"
+    console.log(this);
+    
+}
+
+// chai()
+
+// const addTwo = (num1,num2) => {
+//     return num1 + num2;
+// }
+
+//+++++++++++ Implicit Return Arrow Function
+
+// ++++++++++= Implicit Return means we do not need to use the word or term return . it is a single line code most of the times
+
+ 
+// const addTwo = (num1,num2) => num1 + num2
+// const addTwo = (num1,num2) => (num1 username:"Akif")
+
+const addTwo = (num1,num2) => ({username:"Akif"})
+
+console.log(addTwo(7,3));
+
+// const myArray = [2,3,4,5,6]
+
+// myArray.forEach()
