1df0b4abf78b9b85f5910c27588cab1c59a9eac7
AKIF ANSARI <81532066+dev-AAKIF@users.noreply.github.com>
2025-12-29 12:47:24 +0000

    03 arroy

---
+++ 03_basics/03_arrows.js
@@ -0,0 +1,17 @@
+const user ={
+    username : "Akif",
+    price: 999,
+
+    welcomeMessage: function() {
+        console.log(`${this.username} , welcome to website`);
+        console.log(this);
+        
+        
+    }
+}
+
+user.welcomeMessage()
+user.username = "Sam"
+user.welcomeMessage()
+
+console.log(this);
