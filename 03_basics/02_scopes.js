c2d0109cb5e5be7a48a1c6fb512fce04858aa1a0
AKIF ANSARI <81532066+dev-AAKIF@users.noreply.github.com>
2025-12-29 12:40:14 +0000

    scopes updated

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -6,7 +6,7 @@ if (true) {
 //   var c = 30;
   c = 30
 
-  console.log("Inner :",a);
+  // console.log("Inner :",a);
   
 }
 
@@ -15,6 +15,43 @@ if (true) {
     
 // }
 
-console.log(a);
+// console.log(a);
 // console.log(b);
 // console.log(c);
+function one() {
+  const username = "Akif"
+
+  function two() {
+    const website = "www.google.com"
+    console.log(username);
+  }
+  // console.log(website);
+  two()
+}
+
+// one()
+
+if(true) {
+  const username = "Akboss"
+  if(username === "Akboss") {
+    const website = " github.com"
+    // console.log(username + website);
+  }
+  // console.log(website);
+  
+}
+// console.log(username);
+
+// +++++++++++++++ Interesting +++++++++++++++
+
+console.log(addOne(5));
+
+function addOne(num1) {
+  return num1 + 1
+}
+
+const addTwo = function(num) {
+  return num + 2
+}
+
+addTwo(8)
