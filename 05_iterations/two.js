25f701bfed2f9ec52adbaee34454617d51952315
AKIF ANSARI <81532066+dev-AAKIF@users.noreply.github.com>
2026-01-03 11:12:38 +0000

    two.js itertaions

---
+++ 05_iterations/two.js
@@ -0,0 +1,35 @@
+//while loop
+
+let index = 0
+
+while (index <= 10) {
+    // console.log(`value of i is ${index}`);
+    index = index + 2
+}
+
+// ----------------------------------------------------------
+
+// while loop for array
+
+myArray = ["Flash", "Batman","Superman"]
+
+let arr = 0
+while (arr < myArray.length) {
+    // console.log(`Value is ${myArray[arr]}`);
+    arr = arr + 1
+}
+
+//  ------------------------------
+
+
+
+
+// do while loop
+
+
+let score = 1
+
+do {
+    console.log(`Score is ${score}`);
+    score++
+} while (score <= 10);
