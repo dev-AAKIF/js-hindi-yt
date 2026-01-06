9cce2c08c93d250f2ce1dd6ca297741a209de322
AKIF ANSARI <81532066+dev-AAKIF@users.noreply.github.com>
2026-01-06 19:13:34 +0000

    L30

---
+++ 05_iterations/nine.js
@@ -0,0 +1,39 @@
+// reduce
+
+// reduce requires 3 paramers i.e. 1- Initial Value, 2 - Accumulator, 3 - currentValue
+
+const myNums = [1, 2, 3]
+
+// const myTotal = myNums.reduce( function (acc, currval) {
+//     console.log(`acc is ${acc} and currVal is ${currval}`);
+//     return acc + currval
+// }, 0)
+
+const myTotal = myNums.reduce( (acc, curr) => acc + curr, 0)
+
+// console.log(myTotal);
+
+const shoppingCart = [
+    {
+        itemName: "js course",
+        price: 2999
+    },
+    {
+        itemName: "py course",
+        price: 999
+    },
+    {
+        itemName: "mobile dev course",
+        price: 5999
+    },
+    {
+        itemName: "data science course",
+        price: 12999
+    }
+]
+
+const cartTotal = shoppingCart.reduce( (acc, item) => {
+    return acc + item.price
+}, 0)
+
+console.log(cartTotal);
